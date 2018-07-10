9b45d11953ad23d1cf850ae938ae081a0ef52495
abhi-jha <abhi_jha1995@rediffmail.com>
2018-07-11 00:16:14 +0530

    6

--- main.cpp
+++ main.cpp
@@ -1,81 +1,84 @@
 #include <iostream>
+#include <cmath>
 #include "GL/glew.h"
 #include "GL/freeglut.h"
-void drawpolygon();
-void drawtriangle();
-void drawline();
-void drawpoint();
-void display();
+#define offset 250
+using namespace std;
+GLfloat center_x, center_y, radius;
+void setDrawingColor(GLfloat d = 255.0, GLfloat e = 255.0, GLfloat f = 255.0)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glColor3f(d, e, f);
+}
 void init()
 {
-    glClearColor(0.0,0.0,1.0,0.0);
-    glLoadIdentity();
-    gluOrtho2D(0,500,0,500);
-}
-
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
-    drawpoint();
-    drawline();
-    drawtriangle();
-    drawpolygon();
+    glLoadIdentity();
+    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+void setPixel(GLfloat x, GLfloat y)
+{
+    setDrawingColor(0.0, 255.0, 0.0);
+    glBegin(GL_POINTS);
+    glVertex2f(offset + x, offset + y);
+    glEnd();
     glFlush();
 }
-
-void drawpoint()
+void setAxes()
 {
-    glColor3f(1.0,1.0,1.0);
-    glBegin(GL_POINTS);
-    glVertex2f(100,100);
-    glVertex2f(100,200);
-    glVertex2f(100,300);
-    glVertex2f(100,400);
+    setDrawingColor(0.0, 255.0, 255.0);
+    glBegin(GL_LINE_STRIP);
+    glVertex2i(250, 250);
+    glVertex2i(450, 250);
+    glVertex2i(250, 250);
+    glVertex2i(250, 450);
+    glVertex2i(250, 250);
+    glVertex2i(250, 50);
+    glVertex2i(250, 250);
+    glVertex2i(50, 250);
     glEnd();
+    glFlush();
 }
-
-void drawline()
+void param()
 {
-    glColor3f(1.0,1.0,0.0);
-    glBegin(GL_LINES);
-    glVertex2f(110,183);
-    glVertex2f(200,150);
-    glVertex2f(300,400);
-    glVertex2f(400,400);
-    glEnd();
+    for (GLfloat i =0; i<=360; i = i + 1)
+    {
+        GLfloat a = center_x + radius*GLfloat(cos(3.14*(i / (GLfloat)180)));
+        GLfloat b = center_y + radius*GLfloat(sin(3.14*(i / (GLfloat)180)));
+        cout << "a =" << a << " b = " << b<<"\n";
+        //setDrawingColor(0.0, 255.0, 0.0);
+        //glClear(GL_COLOR_BUFFER_BIT);
+        setPixel(a, b);
+        /*GLfloat c = center_x + radius*GLfloat(cos(3.14*((i +90)/ (GLfloat)180)));
+        GLfloat d = center_y + radius*GLfloat(sin(3.14*((i +90)/ (GLfloat)180)));
+        setDrawingColor(255, 0, 255);
+        setPixel(c, d);
+        GLfloat e = center_x + radius*GLfloat(cos(3.14*((i + 180) / (GLfloat)180)));
+        GLfloat f = center_y + radius*GLfloat(sin(3.14*((i+180) / (GLfloat)180)));
+        setDrawingColor(255, 0, 0);
+        setPixel(e, f);
+        GLfloat g = center_x + radius*GLfloat(cos(3.14*((i + 270) / (GLfloat)180)));
+        GLfloat h = center_y + radius*GLfloat(sin(3.14*((i + 270) / (GLfloat)180)));
+        setDrawingColor(0, 0, 255.0);
+        setPixel(g, h);*/
+    }
 }
-
-void drawtriangle()
+void display()
 {
-    glColor3f(1.0,0.0,0.0);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(125,150);
-    glVertex2f(200,250);
-    glVertex2f(300,270);
-    glEnd();
+    setAxes();
+    param();
 }
-
-void drawpolygon()
+int main(int argc, char **argv)
 {
-    glColor3f(0.0,1.0,1.0);
-    glBegin(GL_POLYGON);
-    glVertex2f(300,150);
-    glVertex2f(400,250);
-    glVertex2f(500,270);
-    glVertex2f(300,110);
-    glEnd();
-}
-
-
-int main(int argc, char** argv)
-{
+    cout << "Enter the coordinates\n";
+    cin >> center_x >> center_y >> radius;
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow (" Primitives ");
-    init ();
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(400, 100);
+    glutCreateWindow("OPENGL EXAMPLE 1");
+    init();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
