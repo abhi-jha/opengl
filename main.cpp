31127df86c597a5cb92fcc2e540a8936ee7e131c
abhi-jha <abhi_jha1995@rediffmail.com>
2018-07-11 00:24:20 +0530

    7

--- main.cpp
+++ main.cpp
@@ -2,84 +2,72 @@
 #include <cmath>
 #include "GL/glew.h"
 #include "GL/freeglut.h"
-#define offset 250
-using namespace std;
-GLfloat center_x, center_y, radius;
-void setDrawingColor(GLfloat d = 255.0, GLfloat e = 255.0, GLfloat f = 255.0)
-{
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glColor3f(d, e, f);
-}
-void init()
-{
-    glMatrixMode(GL_PROJECTION);
+
+void setPixel(int x, int y);
+void ddaline(int x0, int y0, int x1, int y1);
+int roundnum(double number);
+int a, b, c, d;
+
+void init() {
+    glClearColor(0.0, 255.0, 255.0, 0.0);
     glLoadIdentity();
-    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
-    glMatrixMode(GL_MODELVIEW);
+    gluOrtho2D(-300, 300, -300, 300);
 }
-void setPixel(GLfloat x, GLfloat y)
-{
-    setDrawingColor(0.0, 255.0, 0.0);
-    glBegin(GL_POINTS);
-    glVertex2f(offset + x, offset + y);
-    glEnd();
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
     glFlush();
+    glColor3f(1.0, 1.0, 1.0);
+    ddaline(0, -300, 0, 300);
+    ddaline(-300, 0, 300, 0);
+    glColor3f(1.0, 1.0, 0.0);
+    ddaline(a, b, c, d);
 }
-void setAxes()
-{
-    setDrawingColor(0.0, 255.0, 255.0);
-    glBegin(GL_LINE_STRIP);
-    glVertex2i(250, 250);
-    glVertex2i(450, 250);
-    glVertex2i(250, 250);
-    glVertex2i(250, 450);
-    glVertex2i(250, 250);
-    glVertex2i(250, 50);
-    glVertex2i(250, 250);
-    glVertex2i(50, 250);
-    glEnd();
-    glFlush();
-}
-void param()
-{
-    for (GLfloat i =0; i<=360; i = i + 1)
-    {
-        GLfloat a = center_x + radius*GLfloat(cos(3.14*(i / (GLfloat)180)));
-        GLfloat b = center_y + radius*GLfloat(sin(3.14*(i / (GLfloat)180)));
-        cout << "a =" << a << " b = " << b<<"\n";
-        //setDrawingColor(0.0, 255.0, 0.0);
-        //glClear(GL_COLOR_BUFFER_BIT);
-        setPixel(a, b);
-        /*GLfloat c = center_x + radius*GLfloat(cos(3.14*((i +90)/ (GLfloat)180)));
-        GLfloat d = center_y + radius*GLfloat(sin(3.14*((i +90)/ (GLfloat)180)));
-        setDrawingColor(255, 0, 255);
-        setPixel(c, d);
-        GLfloat e = center_x + radius*GLfloat(cos(3.14*((i + 180) / (GLfloat)180)));
-        GLfloat f = center_y + radius*GLfloat(sin(3.14*((i+180) / (GLfloat)180)));
-        setDrawingColor(255, 0, 0);
-        setPixel(e, f);
-        GLfloat g = center_x + radius*GLfloat(cos(3.14*((i + 270) / (GLfloat)180)));
-        GLfloat h = center_y + radius*GLfloat(sin(3.14*((i + 270) / (GLfloat)180)));
-        setDrawingColor(0, 0, 255.0);
-        setPixel(g, h);*/
+
+void ddaline(int x0, int y0, int x1, int y1) {
+    int dy = y1 - y0;
+    int dx = x1 - x0;
+    int steps, i;
+    float xinc, yinc, x = x0, y = y0;
+    if (abs(dx) > abs(dy))
+        steps = abs(dx);
+    else
+        steps = abs(dy);
+    xinc = (float) dx / (float) steps;
+    yinc = (float) dy / (float) steps;
+    setPixel(roundnum(x), roundnum(y));
+    for (i = 0; i < steps; i++) {
+        x += xinc;
+        y += yinc;
+        setPixel(roundnum(x), roundnum(y));
     }
+    glutSwapBuffers();
+    glFlush();
 }
-void display()
-{
-    setAxes();
-    param();
+
+
+int roundnum(double number) {
+    return (number >= 0) ? (int) (number + 0.5) : (int) (number - 0.5);
 }
-int main(int argc, char **argv)
-{
-    cout << "Enter the coordinates\n";
-    cin >> center_x >> center_y >> radius;
+
+void setPixel(int x, int y) {
+    glBegin(GL_POINTS);
+    glVertex2f(x, y);
+    glEnd();
+}
+
+int main(int argc, char **argv) {
+    printf("Enter (x1,y1)\n");
+    scanf("%d%d", &a, &b);
+    printf("Enter (x2,y2)\n");
+    scanf("%d%d", &c, &d);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(400, 100);
-    glutCreateWindow("OPENGL EXAMPLE 1");
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(" dda line ");
     init();
     glutDisplayFunc(display);
     glutMainLoop();
-    return 0;
 }
