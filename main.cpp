19881fe21338ffd34d523241abd0fdcefb0b95ed
abhi-jha <abhi_jha1995@rediffmail.com>
2018-07-10 23:42:34 +0530

    2

--- main.cpp
+++ main.cpp
@@ -1,34 +1,81 @@
 #include <iostream>
 #include "GL/glew.h"
 #include "GL/freeglut.h"
+#define WINDOW_TITLE_PREFIX "Chapter 1"
+int
+        CurrentWidth = 800,
+        CurrentHeight = 600,
+        WindowHandle = 0;
 
-void changeViewPort(int w, int h){
-    std::cout<<" w : "<<w<<" h : "<<h<<"\n";
-    glViewport(0, 0, w, h);
-}
+void Initialize(int, char*[]);
+void InitWindow(int, char*[]);
+void ResizeFunction(int, int);
+void RenderFunction(void);
 
-void render(){
-    std::cout<<"Render\n";
-    glClear(GL_COLOR_BUFFER_BIT |  GL_DEPTH_BUFFER_BIT);
-    glColor3i(255,0,0);
-    for(int  i = 0 ; i < 100; i++)
-        glVertex2i(i,i);
-    glutSwapBuffers();
-}
+int main(int argc, char* argv[])
+{
+    Initialize(argc, argv);
 
-int main(int argc, char* argv[]) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutInitWindowSize(400,500);
-    glutCreateWindow("Hello GL");
-    glutReshapeFunc(changeViewPort);
-    glutDisplayFunc(render);
-
-    GLenum err = glewInit();
-    if(GLEW_OK!= err){
-        fprintf(stderr, "GLEW ERROR");
-        return 1;
-    }
     glutMainLoop();
-    return 0;
+
+    exit(EXIT_SUCCESS);
+}
+
+void Initialize(int argc, char* argv[])
+{
+    InitWindow(argc, argv);
+
+    fprintf(
+            stdout,
+            "INFO: OpenGL Version: %s\n",
+            glGetString(GL_VERSION)
+    );
+
+    glClearColor(0.0f, 1.0f, 0.3f, 0.3f);
+}
+
+void InitWindow(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+
+    glutInitContextVersion(4, 0);
+    glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+
+    glutSetOption(
+            GLUT_ACTION_ON_WINDOW_CLOSE,
+            GLUT_ACTION_GLUTMAINLOOP_RETURNS
+    );
+
+    glutInitWindowSize(CurrentWidth, CurrentHeight);
+
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+    WindowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+    if(WindowHandle < 1) {
+        fprintf(
+                stderr,
+                "ERROR: Could not create a new rendering window.\n"
+        );
+        exit(EXIT_FAILURE);
+    }
+
+    glutReshapeFunc(ResizeFunction);
+    glutDisplayFunc(RenderFunction);
+}
+
+void ResizeFunction(int Width, int Height)
+{
+    CurrentWidth = Width;
+    CurrentHeight = Height;
+    glViewport(0, 0, CurrentWidth, CurrentHeight);
+}
+
+void RenderFunction(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
