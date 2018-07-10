1da3462aa341610f8a4bfe76ab29104bea17fa3b
abhi-jha <abhi_jha1995@rediffmail.com>
2018-07-11 00:05:37 +0530

    5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "GL/glew.h"
 #include "GL/freeglut.h"
+void drawpolygon();
+void drawtriangle();
+void drawline();
+void drawpoint();
+void display();
 void init()
 {
-    glClearColor(0.0,0.0,0.0,0.0);
+    glClearColor(0.0,0.0,1.0,0.0);
     glLoadIdentity();
     gluOrtho2D(0,500,0,500);
 }
@@ -11,8 +16,57 @@ void init()
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    drawpoint();
+    drawline();
+    drawtriangle();
+    drawpolygon();
     glFlush();
 }
+
+void drawpoint()
+{
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_POINTS);
+    glVertex2f(100,100);
+    glVertex2f(100,200);
+    glVertex2f(100,300);
+    glVertex2f(100,400);
+    glEnd();
+}
+
+void drawline()
+{
+    glColor3f(1.0,1.0,0.0);
+    glBegin(GL_LINES);
+    glVertex2f(110,183);
+    glVertex2f(200,150);
+    glVertex2f(300,400);
+    glVertex2f(400,400);
+    glEnd();
+}
+
+void drawtriangle()
+{
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(125,150);
+    glVertex2f(200,250);
+    glVertex2f(300,270);
+    glEnd();
+}
+
+void drawpolygon()
+{
+    glColor3f(0.0,1.0,1.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(300,150);
+    glVertex2f(400,250);
+    glVertex2f(500,270);
+    glVertex2f(300,110);
+    glEnd();
+}
+
 
 int main(int argc, char** argv)
 {
@@ -20,7 +74,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize (500, 500);
     glutInitWindowPosition (100, 100);
-    glutCreateWindow (" Window Name ");
+    glutCreateWindow (" Primitives ");
     init ();
     glutDisplayFunc(display);
     glutMainLoop();
